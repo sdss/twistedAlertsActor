2e04657f62bbe1cbedc42f3579da46042af49db5
johndonor3 <j.donor@tcu.edu>
2018-03-09 14:10:30 -0800

    add files that didnt want to be tracked

---
+++ bin/alerts_main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+# encoding: utf-8
+#
+# file.py
+#
+# Created by José Sánchez-Gallego on 17 Sep 2017.
+
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import logging
+
+from twisted.internet import reactor
+
+from alertsActor import config, __version__
+from alertsActor.logger import log
+from alertsActor.toy_main import alertsActor
+
+import click
+
+
+
+@click.command()
+@click.option('-d', '--debug', is_flag=True, show_default=True,
+              help='Debug mode.')
+def alerts_cmd(debug=False):
+
+    port = config['tron']['port']
+
+    if debug:
+        log.sh.setLevel(logging.DEBUG)
+        log.debug('alertsActor started in debug mode.')
+
+    alertsActor(config, userPort=port, version=__version__)
+
+    reactor.run()
+
+
+if __name__ == '__main__':
+    alerts_cmd()
---
+++ python/alertsActor/alerts_main.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# bmo_actor.py
+#
+# Created by José Sánchez-Gallego on 16 Feb 2017.
+
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import json
+import sys
+import traceback
+
+import time
+
+from click.testing import CliRunner
+
+from RO.StringUtil import strFromException
+from RO.Comm.TwistedTimer import Timer
+from twistedActor import BaseActor, CommandError, UserCmd
+
+from alertsActor import __version__
+from alertsActor.cmds.cmd_parser import alerts_parser
+from alertsActor.logger import log
+
+
+
+class alertsActor(BaseActor):
+    """the actual actor!"""
+
+    def __init__(self, config, **kwargs):
+
+        self.cmdParser = alerts_parser
+        self.config = config
+
+        self.hubConnection = self.connectHub('localhost', datamodel_casts: {actor.keyword: int/fn}, datamodel_callbacks: {actor.keyword: fn})
+
+
+        log.info('starting alertsActor actor version={!r} in port={}'
+                 .format(__version__, kwargs['userPort']))
+
+        super(alertsActor, self).__init__(**kwargs)
+
+        # Sets itself as the default actor to write to when logging.
+        log.set_actor(self)
+
+
+    def parseAndDispatchCmd(self, cmd):
+        """Dispatch the user command."""
+
+        def test_cmd(args):
+            result = CliRunner().invoke(alerts_parser, args)
+            if result.exit_code > 0:
+                # If code > 0, there was an error. We fail the command and inform the users.
+                textMsg = result.output
+                for line in textMsg.splitlines():
+                    line = json.dumps(line).replace(';', '')
+                    cmd.writeToUsers('w', 'text={0}'.format(line))
+                cmd.setState(cmd.Failed)
+                return False
+            else:
+                if '--help' in args:
+                    # If help was in the args, we just want to print the usage to the users.
+                    textMsg = result.output
+                    for line in textMsg.splitlines():
+                        line = json.dumps(line).replace(';', '')
+                        cmd.writeToUsers('w', 'text={0}'.format(line))
+                    cmd.setState(cmd.Done)
+                    return False
+
+                return True
+
+        if not cmd.cmdBody:
+            # echo to show alive
+            self.writeToOneUser(":", "", cmd=cmd)
+            return
+
+        cmd.setState(cmd.Running)
+
+        try:
+            result = test_cmd(cmd.cmdBody.split())
+            if result is False:
+                return
+            alerts_parser(cmd.cmdBody.split(), obj=dict(actor=self, cmd=cmd))
+        except CommandError as ee:
+            cmd.setState('failed', textMsg=strFromException(ee))
+            return
+        except Exception as ee:
+            sys.stderr.write('command {0!r} failed\n'.format(cmd.cmdStr))
+            traceback.print_exc(file=sys.stderr)
+            textMsg = strFromException(ee)
+            hubMsg = 'Exception={0}'.format(ee.__class__.__name__)
+            cmd.setState("failed", textMsg=textMsg, hubMsg=hubMsg)
+        except BaseException:
+            # This catches the SystemExit that Click insists in returning.
+            pass
